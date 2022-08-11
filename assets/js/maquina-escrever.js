0ad2efb7eb1ceb8ce3e821f409989861d6c40893
Vagner Turella <100965375+vagnerturella@users.noreply.github.com>
2022-08-11 14:09:12 -0300

    Add files via upload

---
+++ assets/js/maquina-escrever.js
@@ -0,0 +1,12 @@
+//Maquina de escrever
+function typeWrite(elemento) {
+    const textoArray = elemento.innerHTML.split("");
+    elemento.innerHTML = " ";
+    textoArray.forEach(function (letra, i) {
+      setTimeout(function () {
+        elemento.innerHTML += letra;
+      }, 100 * i);
+    });
+  }
+  const titulo = document.querySelector(".maquina-escrever");
+  typeWrite(titulo);
---
+++ assets/js/scrollReaveal.js
@@ -0,0 +1,141 @@
+//Funcionar de baixo pra cima
+window.sr = ScrollReveal({reset: true})
+
+//Classes e funcoes
+ScrollReveal().reveal('.intro-img', {delay: 400});
+ScrollReveal().reveal('.intro-txt', {delay: 100});
+ScrollReveal().reveal('.intro-btn1', {
+    delay: 1000,
+    rotate:{
+        x:200,
+        y:0,
+        z:0
+    }
+});
+ScrollReveal().reveal('.intro-btn2', {
+    delay: 1400,
+    rotate:{
+        x:200,
+        y:0,
+        z:0
+    }
+});
+ScrollReveal().reveal('.intro-btn3', {
+    delay: 1700,
+    rotate:{
+        x:200,
+        y:0,
+        z:0
+    }
+});
+ScrollReveal().reveal('.about-txt', {delay: 600});
+ScrollReveal().reveal('.about-txt h1', {delay: 200});
+ScrollReveal().reveal('.about-img', {delay: 500});
+ScrollReveal().reveal('.projects-area h1', {delay: 200});
+ScrollReveal().reveal('.card1', {
+    delay: 400,
+    rotate:{
+        x:0,
+        y:300,
+        z:0
+    }
+});
+ScrollReveal().reveal('.card2', {
+    delay: 800,
+    rotate:{
+        x:0,
+        y:300,
+        z:0
+    }
+});
+ScrollReveal().reveal('.card3', {
+    delay: 1200,
+    rotate:{
+        x:0,
+        y:300,
+        z:0
+    }
+});
+ScrollReveal().reveal('.card4', {
+    delay: 1600,
+    rotate:{
+        x:0,
+        y:300,
+        z:0
+    }
+});
+ScrollReveal().reveal('.card5', {
+    delay: 2000,
+    rotate:{
+        x:0,
+        y:300,
+        z:0
+    }
+});
+ScrollReveal().reveal('.card6', {
+    delay: 2400,
+    rotate:{
+        x:0,
+        y:300,
+        z:0
+    }
+});
+ScrollReveal().reveal('.projects-area a', {delay: 400});
+ScrollReveal().reveal('.knowledge-area h1', {delay: 200});
+ScrollReveal().reveal('#html', {
+    delay: 300,
+    rotate:{
+        x:0,
+        y:200,
+        z:0
+    }
+});
+ScrollReveal().reveal('#css', {
+    delay: 600,
+    rotate:{
+        x:0,
+        y:200,
+        z:0
+    }
+});
+ScrollReveal().reveal('#js', {
+    delay: 900,
+    rotate:{
+        x:0,
+        y:200,
+        z:0
+    }
+});
+ScrollReveal().reveal('#bt', {
+    delay: 1200,
+    rotate:{
+        x:0,
+        y:200,
+        z:0
+    }
+});
+ScrollReveal().reveal('#git', {
+    delay: 1500,
+    rotate:{
+        x:0,
+        y:200,
+        z:0
+    }
+});
+ScrollReveal().reveal('#github', {
+    delay: 1800,
+    rotate:{
+        x:0,
+        y:200,
+        z:0
+    }
+});
+ScrollReveal().reveal('.txt-container', {delay: 600});
+
+
+
+
+
+
+
+
